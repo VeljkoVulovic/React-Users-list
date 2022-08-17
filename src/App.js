f837f746cc7767788ab58b9de2d8260a23e5c5eb
Veljko <veljkovulovic1@gmail.com>
2022-08-18 00:08:07 +0200

    React Users list

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+
+import AddUser from './components/Users/AddUser';
+import UsersList from "./components/Users/UsersList";
+
+
+function App() {
+    const [usersList, setUsersList] = useState([]);
+
+    const addUserHandler = (name, age) => {
+        setUsersList((prevUsersList) => {
+            return [...prevUsersList, {name: name, age: age, id: Math.random().toString()}];
+        });
+    }
+
+    return (
+        <div>
+            <AddUser onAddUser={addUserHandler}/>
+            {usersList.length !== 0 && <UsersList users={usersList}/>}
+        </div>
+    );
+}
+
+export default App;
